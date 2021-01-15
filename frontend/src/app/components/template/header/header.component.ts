c93b3e3d6bb6608a317a267023efd183fcb137ee
Karine <martinskazk@gmail.com>
2021-01-14 21:04:02 -0300

    Atualizar títulos

--- frontend/src/app/components/template/header/header.component.ts
+++ frontend/src/app/components/template/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _headerService: HeaderService,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  public get title(): string {
+   return this._headerService.headerData.title;
+  }
+
+  public get icon(): string {
+    return this._headerService.headerData.icon;
+   }
+
+  public get routeUrl(): string {
+    return this._headerService.headerData.routeUrl;
+   }
+  
 
 }
